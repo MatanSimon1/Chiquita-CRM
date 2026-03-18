8a846b699bbfe2ed31442ef488ef31ac43efd26b
MatanSimon1 <96584976+MatanSimon1@users.noreply.github.com>
2026-03-18 14:58:29 +0200

    Update users.js

--- users.js
+++ users.js
@@ -3,7 +3,7 @@ const APPS_SCRIPT_URL = 'https://script.google.com/macros/s/AKfycbzF1EKN09oQuh6m
 // הגדרת הגיליון של Chiquita
 const CHIQUITA_CONFIG = {
   sheetId: '1BzTKhSdRhfANtbCCZKVYgcqg8hCBqJpVAWDRf-2vLjg',
-  sheetTab: 'לידים CHIQUITA - צאירי',
+  sheetTab: 'Chiquita Leads',
   avgMonths: 5,
   name: 'Chiquita'
 };
